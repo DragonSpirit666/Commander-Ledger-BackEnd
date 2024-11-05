6b30b370a9655da764f8180d317632bdd5986bf7
Paul Labelle-Bilodeau <0627179@cstjean.qc.ca>
2024-11-05 11:36:34 -0500

    test register postman (à modifier pour envoyer une réponse json)

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,56 +5,37 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-use Illuminate\View\View;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display the registration view.
-     */
-    public function create(): View
-    {
-        return view('auth.register');
-    }
-
     /**
      * Handle an incoming registration request.
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request): Response
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'photo' => ['nullable', 'string', 'max:255'],
-            'prive' => ['nullable', 'boolean'],
-            'nb_parties_gagnees' => ['nullable', 'integer'],
-            'nb_parties_perdues' => ['nullable', 'integer'],
-            'prix_total_decks' => ['nullable', 'numeric'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'photo' => $request->photo,
-            'prive' => $request->prive,
-            'nb_parties_gagnees' => $request->nb_parties_gagnees,
-            'nb_parties_perdues' => $request->nb_parties_perdues,
-            'prix_total_decks' => $request->prix_total_decks,
+            'password' => Hash::make($request->string('password')),
         ]);
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return response()->noContent();
     }
 }
